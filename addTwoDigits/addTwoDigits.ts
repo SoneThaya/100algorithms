28092a3ec6bbd439c1391ebd6669c745a1ca33be
soggy <sthaya@gmail.com>
2019-05-10 20:06:09 -0700

    solved problem

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,14 @@
 function addTwoDigits(n: any): number {
+    const nums = n.toString().split('');
 
+    return parseInt(nums[0]) + parseInt(nums[1]);
 }
 
-// console.log(addTwoDigits(29));
+console.log(addTwoDigits(29));
+
+function addTwoDigits2(num: any): number {
+    let arr: string[] = num.toString().split('');
+    return (+arr[0]) + (+arr[1])
+}
+
+console.log(addTwoDigits2(29));
