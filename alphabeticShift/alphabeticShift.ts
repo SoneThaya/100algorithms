e0c26c5508a1bfe2be66b8a3bf35dcac420d90a2
soggy <sthaya@gmail.com>
2019-05-10 21:35:59 -0700

    solved problem

--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,21 @@
-function alphabeticShift(inputString: string): string {
+function alphaShift(str) {
+  
+    let newStr = str.replace(/[a-z]/gi, char => {
+      if(char == 'z'){
+        return 'a';
+      } else {
+        return String.fromCharCode(char.charCodeAt(0) + 1);
+      }
+    })
+    return newStr;
+  }
+   
+  console.log(alphaShift('crazy'));
 
-}
 
-console.log(alphabeticShift('crazy'));
+  //solution 2
+
+  function alphabeticShift2(i) {
+    return i.split("").map(a=>a.charCodeAt(0)+1).map(b=>String.fromCharCode(b)).join("").replace(/{/g, "a")}
+
+    console.log(alphabeticShift2('crazy'));
