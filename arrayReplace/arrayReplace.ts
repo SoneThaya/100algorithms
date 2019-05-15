2a8006d6ede6526158559e424599fba98d89e264
soggy <sthaya@gmail.com>
2019-05-14 22:15:45 -0700

    solved problem

--- arrayReplace/arrayReplace.ts
+++ arrayReplace/arrayReplace.ts
@@ -1,5 +1,23 @@
 function arrayReplace(inputArray: number[], elemToReplace: number, substitutionElem: number): number[] {
-   
+   inputArray.forEach((element, index) => {
+       if(element === elemToReplace){
+           inputArray[index] = substitutionElem;
+       }
+   })
+   return inputArray;
 }
 
 console.log(arrayReplace([1, 2, 1], 1, 3));
+
+// solution 2
+
+function arrayReplace2(arr: number[], targetElement: number, newElement: number) {
+ 
+    arr.forEach((currentValue, index, actualArray) => { 
+        actualArray[index] === targetElement ? actualArray[index] = newElement : actualArray[index] = currentValue; 
+    });
+ 
+    return arr;
+}
+ 
+console.log(arrayReplace2([1,2,1, 8, 1], 1, 3));
