8925a401bcb070790c878ce612a7e746d6d3b242
soggy <sthaya@gmail.com>
2019-05-18 16:35:57 -0700

    solved problem

--- caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
+++ caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
@@ -1,6 +1,20 @@
 function isCaseInsensitivePalindrome(inputString: string): boolean {
+    const originalLowerCase = inputString.toLowerCase();
+    const reversedWord = originalLowerCase.toLowerCase().split('').reverse().join('');
 
+    return originalLowerCase === reversedWord;
 }
 
 console.log(isCaseInsensitivePalindrome('AaBaa'));
 console.log(isCaseInsensitivePalindrome('abac'));
+
+// solution 2
+
+function caseInsensitivePalindrome(word: string): boolean {
+    if (word.length <= 1) return true;
+    return word.split('').reverse().join('').toLowerCase() === word.toLowerCase();
+}
+ 
+console.log(caseInsensitivePalindrome('AaBaa'));
+console.log(caseInsensitivePalindrome('aabaa'));
+console.log(caseInsensitivePalindrome('abaac'));
