8c8f31ffae7317674c70c265a5bd9ca9a122f156
soggy <sthaya@gmail.com>
2019-05-20 09:39:22 -0700

    solved problem

--- checkPalindrome/checkPalindrome.ts
+++ checkPalindrome/checkPalindrome.ts
@@ -1,7 +1,36 @@
 function checkPalindrome(inputString: string): boolean {
+    const originalWord = inputString.toLowerCase();
+    const reversedWord = originalWord.split('').reverse().join('');
 
+    return originalWord === reversedWord;
 }
 
 console.log(checkPalindrome('aabaa'));
 console.log(checkPalindrome('abac'));
 console.log(checkPalindrome('a'));
+
+// solution 2
+
+function checkPalindrome2(word: string): boolean {
+ 
+    let start: number = 0;
+    let end: number = word.length - 1;
+ 
+    while (start !== end) {
+        if (word[start] === word[end]){
+            start++;
+            end--;
+        } else {
+            break;
+        }
+    }
+ 
+    return start === end;
+}
+ 
+ 
+console.log(checkPalindrome2('aabaa'));
+console.log(checkPalindrome2('abac'));
+console.log(checkPalindrome2('a'));
+console.log(checkPalindrome2('palindrome'));
+console.log(checkPalindrome2('level'));
