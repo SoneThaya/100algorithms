46e9eed191d46f237b89c27addcf312b7fc32b7d
soggy <sthaya@gmail.com>
2019-05-23 18:43:35 -0700

    solved problem

--- confirmEnding/confirmEnding.ts
+++ confirmEnding/confirmEnding.ts
@@ -1,6 +1,17 @@
 function confirmEnding(str: string, target: string) {
-   
+   const start = str.length - target.length;
+
+   return str.substr(start) === target;
 }
 
 console.log(confirmEnding("Abstraction", "action"));
 console.log(confirmEnding("Open sesame", "pen"));
+
+// solution 2
+
+function confirmEnding2(bigWord: string, smallWord: string): boolean {
+    return bigWord.substring(bigWord.length - smallWord.length) === smallWord;
+}
+ 
+console.log(confirmEnding2('Abstraction', 'action'));
+console.log(confirmEnding2('Open sesame', 'pen'));
