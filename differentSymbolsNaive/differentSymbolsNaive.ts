aa1e59b0792d013a4984aae27e36cfdeeb04c30c
soggy <sthaya@gmail.com>
2019-05-25 18:48:39 -0700

    solved problem

--- differentSymbolsNaive/differentSymbolsNaive.ts
+++ differentSymbolsNaive/differentSymbolsNaive.ts
@@ -1,5 +1,27 @@
 function differentSymbolsNaive(s: string): number {
- 
+    const uniqueChars = new Set();
+    const inputChars = s.split('');
+
+    inputChars.forEach((char) => {
+        uniqueChars.add(char);
+    })
+
+    return uniqueChars.size;
 }
 
 console.log(differentSymbolsNaive('cabca'));
+
+// solution 2
+
+function differentSymbolsNaive2(s: string): number {
+    const uniqueChars = {};
+    const inputChars = s.split('');
+
+    inputChars.forEach((char) => {
+        uniqueChars[char] = 1;
+    })
+
+    return Object.keys(uniqueChars).length;
+}
+
+console.log(differentSymbolsNaive2('cabca'));
