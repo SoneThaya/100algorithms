92f840251fe7d26979b0cc36d376dfa14307bfb3
soggy <sthaya@gmail.com>
2019-05-25 18:37:50 -0700

    solved problem

--- depositProfit/depositProfit.ts
+++ depositProfit/depositProfit.ts
@@ -1,5 +1,28 @@
 function depositProfit(deposit: number, rate: number, threshold: number): number {
- 
+    let year = 0;
+    let account = deposit;
+
+    while (threshold > account) {
+        account += account * (rate / 100);
+        year++;
+    }
+
+    return year;
 }
 
 console.log(depositProfit(100, 20, 170))
+
+// solution 2
+
+function depositProfit2(deposit: number, rate: number, threshold: number): number {
+ 
+    let years = 0;
+ 
+    while (deposit < threshold) {
+        deposit += deposit * (rate / 100);
+        years++;
+    }
+ 
+    return years;
+}
+console.log(depositProfit2(100, 20, 170));
