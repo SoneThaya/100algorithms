f7230df81380250ab0cf16bd70a56e350187cbdb
Sone <sthaya@gmail.com>
2019-05-27 16:44:55 -0700

    solved problem

--- encloseInBrackets/encloseInBrackets.ts
+++ encloseInBrackets/encloseInBrackets.ts
@@ -1,5 +1,10 @@
 function encloseInBrackets(inputString: string): string {
-
+    return `(${inputString})`;
 }
 
 console.log(encloseInBrackets('abacaba'));
+
+function encloseInBrackets2(str:string): string {
+    return `(${str})`;
+}
+console.log(encloseInBrackets2('abacaba'));
