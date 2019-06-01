d9a462f83be4d3f359c0981dce7b57de29d1642f
Sone <sthaya@gmail.com>
2019-05-31 18:58:48 -0700

    solved problem

--- firstDigit/firstDigit.ts
+++ firstDigit/firstDigit.ts
@@ -1,5 +1,12 @@
 function firstDigit(inputString: string): string {
+    const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    const chars = inputString.split('');
 
+    for (let i = 0; i < inputString.length; i++) {
+        if (digits.includes(inputString[i])) {
+            return inputString[i];
+        }
+    }
 }
 
 console.log(firstDigit('var_1__Int'));
