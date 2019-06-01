846934cf3a7d955c290ef7f2494267209985a52d
Sone <sthaya@gmail.com>
2019-05-31 18:51:55 -0700

    solved problem

--- findEmailDomain/findEmailDomain.ts
+++ findEmailDomain/findEmailDomain.ts
@@ -1,6 +1,19 @@
 function findEmailDomain(address: string): string {
+    const lastAtIndex = address.lastIndexOf('@');
 
+    return address.slice(lastAtIndex + 1, address.length);
 }
 
 console.log(findEmailDomain('prettyandsimple@example.com'));
 console.log(findEmailDomain('<>[]:,;@\"!#$%&*+-/=?^_{}| ~.a\"@example.org'));
+
+// solution 2
+
+function findEmailDomain2(email: string): string {
+ 
+    return email.slice(email.lastIndexOf('@') + 1);
+ 
+}
+ 
+console.log(findEmailDomain2('prettyandsimple@example.com'));
+console.log(findEmailDomain2('<>[];@!#%$&*+=;?^_{}||\/.@example.org'));
