d575aa01ab56b5656ff9ae6353693f94166e0b63
Sone <sthaya@gmail.com>
2019-06-02 14:48:03 -0700

    solved problem

--- houseOfCats/houseOfCats.ts
+++ houseOfCats/houseOfCats.ts
@@ -1,6 +1,34 @@
 function houseOfCats(legs: number): number[] {
+    const peopleCount: number [] = [];
 
+    while (legs >= 0) {
+        peopleCount.unshift(legs / 2);
+
+        legs -= 4;
+    }
+
+    return peopleCount;
 }
 
 console.log(houseOfCats(6));
 console.log(houseOfCats(2));
+
+// solution 2
+
+function houseOfCats2(numOfLegs: number): number[] {
+ 
+    let possibleCombinations: number[] = [];
+    return possibleNumOfHumanLegs(numOfLegs);
+ 
+    
+    
+    function possibleNumOfHumanLegs(num) {
+        if (num <= 0) return possibleCombinations;
+        possibleCombinations.push(num / 2);
+        return possibleNumOfHumanLegs(num - 4);
+    }
+}
+ 
+console.log(houseOfCats2(6));
+console.log(houseOfCats2(2));
+console.log(houseOfCats2(10));
