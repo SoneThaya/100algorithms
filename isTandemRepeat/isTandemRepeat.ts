1cee43f1338a07178a0c911512eb2da6465c054e
Sone <sthaya@gmail.com>
2019-06-03 18:19:12 -0700

    solved problem

--- isTandemRepeat/isTandemRepeat.ts
+++ isTandemRepeat/isTandemRepeat.ts
@@ -1,5 +1,14 @@
 function isTandemRepeat(inputString: string): boolean {
-  
+    const wordLength = inputString.length;
+
+    if (wordLength % 2 === 0) {
+        const firstHalf = inputString.slice(0, wordLength / 2);
+        const secondHalf = inputString.slice(wordLength / 2, wordLength);
+
+        return firstHalf === secondHalf;
+    }
+
+    return false;
 }
 
 console.log(isTandemRepeat('tandemtandem'))
