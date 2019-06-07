ea5f504510607ee325b7e779f3708932a2705924
Sone <sthaya@gmail.com>
2019-06-06 17:36:53 -0700

    solved problem

--- maxMultiple/maxMultiple.ts
+++ maxMultiple/maxMultiple.ts
@@ -1,5 +1,18 @@
 function maxMultiple(divisor: number, bound: number): number  {
+    let remainder = bound % divisor;
 
+    return bound - remainder;
 }
 
-console.log(maxMultiple(3,10));
+console.log(maxMultiple2(3,10));
+
+// solution 2
+
+function maxMultiple2(divisor: number, bound: number): number {
+    return divisor * Math.floor(bound / divisor);
+}
+ 
+console.log(maxMultiple(3, 10));
+console.log(maxMultiple(3, 30));
+console.log(maxMultiple(3, 29));
+console.log(maxMultiple(3, 16));
