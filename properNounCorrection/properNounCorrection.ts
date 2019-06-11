6d57618e761c31d700c65c0aedd53c189361f022
Sone <sthaya@gmail.com>
2019-06-10 19:27:34 -0700

    solved problem

--- properNounCorrection/properNounCorrection.ts
+++ properNounCorrection/properNounCorrection.ts
@@ -1,6 +1,19 @@
 function properNounCorrection(noun: string): string {
-   
+   const firstChar = noun[0].toUpperCase();
+   const restOfWord = noun.slice(1, noun.length).toLowerCase();
+
+   return firstChar.concat(restOfWord);
 }
 
 console.log(properNounCorrection('pARiS'));
 console.log(properNounCorrection('John'));
+
+// solution 2
+
+function properNounCorrection2(str: string): string {
+  return str.slice(0,1).toUpperCase() + str.slice(1).toLowerCase();
+}
+
+
+console.log(properNounCorrection2('pARiS'));
+console.log(properNounCorrection2('John'));
