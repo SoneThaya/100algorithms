78de91ee34a9f171472755e1fda177dc154d65dd
Sone <sthaya@gmail.com>
2019-06-12 17:32:38 -0700

    solved problem

--- reflectString/reflectString.ts
+++ reflectString/reflectString.ts
@@ -1,5 +1,32 @@
 function reflectString(inputString: string): string {
-  
+  const chars: string[] = inputString.split('');
+  const reflectionLetters = {
+    'a': 'z', 'b': 'y', 'c': 'x', 'd': 'w', 'e': 'v', 'f': 'u', 'g': 't', 'h': 's', 'i': 'r', 'j': 'q', 'k': 'p', 'l': 'o', 'm': 'n', 
+      'n': 'm', 'o': 'l', 'p': 'k', 'q': 'j', 'r': 'i', 's': 'h', 't': 'g', 'u': 'f', 'v': 'e', 'w': 'd', 'x': 'c', 'y': 'b', 'z': 'a'
+  };
+
+  let reflectedWord = '';
+
+  chars.forEach((char) => {
+    reflectedWord += reflectionLetters[char];
+  });
+
+  return reflectedWord;
 }
 
 console.log(reflectString("name"));
+
+// solution 2
+
+function reflectString2(str: string): string {
+ 
+  let alphabet = {
+      'a': 'z', 'b': 'y', 'c': 'x', 'd': 'w', 'e': 'v', 'f': 'u', 'g': 't', 'h': 's', 'i': 'r', 'j': 'q', 'k': 'p', 'l': 'o', 'm': 'n', 
+      'n': 'm', 'o': 'l', 'p': 'k', 'q': 'j', 'r': 'i', 's': 'h', 't': 'g', 'u': 'f', 'v': 'e', 'w': 'd', 'x': 'c', 'y': 'b', 'z': 'a'
+  };
+
+  return str.split('').map((element) => { return alphabet[element]}).join('');
+
+}
+
+console.log(reflectString2('name'));
