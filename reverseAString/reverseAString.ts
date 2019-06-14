e38729c8bff85813cdf6ee12dac40b5a32ee78c2
Sone <sthaya@gmail.com>
2019-06-13 18:03:06 -0700

    solved problem

--- reverseAString/reverseAString.ts
+++ reverseAString/reverseAString.ts
@@ -1,6 +1,21 @@
 function reverseAString(str: string): string {
-
+  return str.split('').reverse().join('');
 }
 
 console.log(reverseAString('hello'));
 console.log(reverseAString('Howdy'));
+
+// solution 2
+
+function reverseAString2(str: string): string {
+  let reversedWord = '';
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedWord += str[i];
+  }
+
+  return reversedWord;
+}
+
+console.log(reverseAString2('hello'));
+console.log(reverseAString2('Howdy'));
