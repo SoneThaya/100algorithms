33da08668ec95281718e22fd93304e909ded82e8
Sone <sthaya@gmail.com>
2019-06-14 18:06:41 -0700

    solved problem

--- seatsInTheater/seatsInTheater.ts
+++ seatsInTheater/seatsInTheater.ts
@@ -1,5 +1,18 @@
 function seatsInTheater(nCols: number, nRows: number, col: number, row: number): number {
+  const colDiff = nCols - col + 1;
+  const rowDiff = nRows - row;
 
+  return colDiff * rowDiff;
 }
 
 console.log(seatsInTheater(16, 11, 5, 3));
+
+// solution 2
+
+function seatsInTheater2(nCols: number, nRows: number, col: number, row: number): number {
+ 
+  return ((nCols - col) + 1) * (nRows - row);
+
+}
+
+console.log(seatsInTheater2(16,11,5,3));
