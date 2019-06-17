e782b1de15af7f23851e2cd17022be01424e3432
Sone <sthaya@gmail.com>
2019-06-16 17:51:11 -0700

    solved problem

--- shapeArea/shapeArea.ts
+++ shapeArea/shapeArea.ts
@@ -1,6 +1,23 @@
 function shapeArea(n: number): number {
+  let area = 1;
 
+  for (let i = 1; i < n; i++) {
+    area += 4 * i;
+  }
+
+  return area;
 }
 
 console.log(shapeArea(2));
 console.log(shapeArea(3));
+
+// solution 2
+
+function shapeArea2(value: number): number {
+  return Math.pow(value,2) + Math.pow(value - 1, 2);
+}
+
+console.log(shapeArea2(1));
+console.log(shapeArea2(2));
+console.log(shapeArea2(3));
+console.log(shapeArea2(4));
