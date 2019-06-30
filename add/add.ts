8d25261b8fa438ddf6699b4a66b4c2466da1eb3b
Sone <sthaya@gmail.com>
2019-06-30 15:31:45 -0700

    solved problem

--- add/add.ts
+++ add/add.ts
@@ -17,3 +17,18 @@ console.log(add(3, 2));
 console.log(add2(1,2,3,4,5));
 console.log(add2(2,3));
 
+// solution 2
+
+function add3(param1: number, param2: number): number {
+    return param1 + param2;
+}
+ 
+function add4(...param1: number[]): number {
+    return param1.reduce((a, b) => a + b);
+}
+ 
+console.log(add3(1, 2)); // 3
+console.log(add3(3, 2)); // 5
+ 
+console.log(add4(1, 2, 3, 4, 5)); // 15
+console.log(add4(2, 3)); // 
