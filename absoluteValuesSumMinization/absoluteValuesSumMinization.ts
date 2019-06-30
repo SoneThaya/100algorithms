be947ba32acdaf8441fd057946b1a5f03f197f06
Sone <sthaya@gmail.com>
2019-06-29 18:18:01 -0700

    updated solution

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -8,3 +8,40 @@ console.log(absoluteValuesSumMinimization([2, 4, 7]));
 console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
 console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
 console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
+
+// solution 2
+
+function absoluteValuesSumMinimization2(a: number[]): number {
+    return a.reduce((x, y) => {
+        const absSumX = a.reduce((b, c) => {
+            return (c > x) ? b + (c - x) : b + -(c - x);
+        }, 0);
+        const absSumY = a.reduce((d, e) => {
+            return (e > y) ? d + (e - y) : d + -(e - y);
+        }, 0);
+        if (absSumX <= absSumY) {
+            return x;
+        }
+        return y;
+    });
+}
+ 
+console.log(absoluteValuesSumMinimization2([2, 4, 7])); // 4
+console.log(absoluteValuesSumMinimization2([2, 4, 7, 6])); // 4
+console.log(absoluteValuesSumMinimization2([2, 4, 7, 6, 6])); // 6
+console.log(absoluteValuesSumMinimization2([2, 4, 7, 6, 6, 8])); // 6
+
+// solution 3
+
+function absoluteValuesSumMinimization3(a : number[]): number {
+    a.sort();
+    let median = Math.floor(a.length / 2);
+    return a.length % 2 == 0 ? a[median - 1] : a[median];  
+}
+ 
+ 
+ 
+console.log(absoluteValuesSumMinimization3([2,4,7]));
+console.log(absoluteValuesSumMinimization3([2,4,7, 6]));
+console.log(absoluteValuesSumMinimization3([2, 4, 7, 6, 6]));
+console.log(absoluteValuesSumMinimization3([2,4,7, 6, 6, 8]));
