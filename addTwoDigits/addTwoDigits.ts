2d1d353a629a51139d5ed31cff7efab4ca0df5a0
Sone <sthaya@gmail.com>
2019-07-02 17:29:02 -0700

    second solution

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -12,3 +12,15 @@ function addTwoDigits2(num: any): number {
 }
 
 console.log(addTwoDigits2(29));
+
+// solution 2
+function addTwoDigits3(num: number): number {
+    let arr: string[] = num.toString().split('');
+    return (+arr[0]) + (+arr[1]);
+}
+ 
+ 
+ 
+console.log(addTwoDigits3(29));
+console.log(addTwoDigits3(11));
+console.log(addTwoDigits3(44));
