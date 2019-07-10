bfe8b5311291be64ef0b946b4b240728f3fd8dfd
Sone <sthaya@gmail.com>
2019-07-09 18:05:29 -0700

    solved problem

--- areEquallyStrong/areEquallyStrong.ts
+++ areEquallyStrong/areEquallyStrong.ts
@@ -1,25 +1,55 @@
-function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
-    const yourWeakest = yourLeft <= yourRight ? yourLeft : yourRight;
-    const yourStrongest = yourLeft >= yourRight ? yourLeft : yourRight;
-    const friendsWeakest = friendsLeft <= friendsRight ? friendsLeft : friendsRight;
-    const friendsStrongest = friendsLeft >= friendsRight ? friendsLeft : friendsRight;
+function areEquallyStrong(
+  yourLeft: number,
+  yourRight: number,
+  friendsLeft: number,
+  friendsRight: number
+): boolean {
+  const yourWeakest = yourLeft <= yourRight ? yourLeft : yourRight;
+  const yourStrongest = yourLeft >= yourRight ? yourLeft : yourRight;
+  const friendsWeakest =
+    friendsLeft <= friendsRight ? friendsLeft : friendsRight;
+  const friendsStrongest =
+    friendsLeft >= friendsRight ? friendsLeft : friendsRight;
 
-    return yourStrongest === friendsStrongest && yourWeakest === friendsWeakest;
+  return yourStrongest === friendsStrongest && yourWeakest === friendsWeakest;
 }
 
-console.log(areEquallyStrong(10, 15, 15, 10))
-console.log(areEquallyStrong(15, 10, 15, 10))
-console.log(areEquallyStrong(15, 10, 15, 9))
+console.log(areEquallyStrong(10, 15, 15, 10));
+console.log(areEquallyStrong(15, 10, 15, 10));
+console.log(areEquallyStrong(15, 10, 15, 9));
 
-function areEquallyStrong2(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
-    if((yourLeft + yourRight) === (friendsLeft + friendsRight)){
-        return true;
-    } else {
-        return false;
-    }
+// SOLUTION 2
+
+function areEquallyStrong2(
+  yourLeft: number,
+  yourRight: number,
+  friendsLeft: number,
+  friendsRight: number
+): boolean {
+  if (yourLeft + yourRight === friendsLeft + friendsRight) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-console.log(areEquallyStrong2(10, 15, 15, 10))
-console.log(areEquallyStrong2(15, 10, 15, 10))
-console.log(areEquallyStrong2(15, 10, 15, 9))
+console.log(areEquallyStrong2(10, 15, 15, 10));
+console.log(areEquallyStrong2(15, 10, 15, 10));
+console.log(areEquallyStrong2(15, 10, 15, 9));
 
+function areEquallyStrong3(
+  yourLeft: number,
+  yourRight: number,
+  friendsLeft: number,
+  friendsRight: number
+): boolean {
+  if (yourLeft + yourRight === friendsLeft + friendsRight) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(areEquallyStrong3(10, 15, 15, 10));
+console.log(areEquallyStrong3(15, 10, 15, 10));
+console.log(areEquallyStrong3(15, 10, 15, 9));
