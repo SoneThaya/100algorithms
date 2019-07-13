a67d24e8b9bb19ed344c6b6975bb2b265db4694c
Sone <sthaya@gmail.com>
2019-07-12 17:22:17 -0700

    solved problem

--- arrayConversion/arrayConversion.ts
+++ arrayConversion/arrayConversion.ts
@@ -1,28 +1,28 @@
 function arrayConversion(inputArray: number[]): number {
-    let isOdd = true;
+  let isOdd = true;
 
-    while(inputArray.length !== 1){
-        inputArray = sumProduct(inputArray, isOdd);
-        isOdd = !isOdd;
-    }
+  while (inputArray.length !== 1) {
+    inputArray = sumProduct(inputArray, isOdd);
+    isOdd = !isOdd;
+  }
 
-    return inputArray[0];
+  return inputArray[0];
 }
 
-function sumProduct(nums: number[], isOdd: boolean): number[]{
-    const sumProducts: number[] = [];
+function sumProduct(nums: number[], isOdd: boolean): number[] {
+  const sumProducts: number[] = [];
 
-    if(isOdd){
-        for(let i = 0; i < nums.length; i += 2){
-            sumProducts.push(nums[i] + nums[i + 1]);
-        }
-    } else {
-        for (let i = 0; i < nums.length; i += 2){
-            sumProducts.push(nums[i] * nums[i + 1]);
-        }
+  if (isOdd) {
+    for (let i = 0; i < nums.length; i += 2) {
+      sumProducts.push(nums[i] + nums[i + 1]);
     }
+  } else {
+    for (let i = 0; i < nums.length; i += 2) {
+      sumProducts.push(nums[i] * nums[i + 1]);
+    }
+  }
 
-    return sumProducts;
+  return sumProducts;
 }
 
 console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]));
@@ -30,22 +30,54 @@ console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]));
 // solution 2
 
 function arrayConversion2(arr: number[]): number {
-    function reduce(arr){
-        let tempArray: number[] = [];
-        for (let i = arr.length; i > 0; i -= 2){
-            action === true ? tempArray.push(arr.pop() + arr.pop()) : tempArray.push(arr.pop() * arr.pop());                
-        }
-        action = !action;
-        return tempArray;
+  function reduce(arr) {
+    let tempArray: number[] = [];
+    for (let i = arr.length; i > 0; i -= 2) {
+      action === true
+        ? tempArray.push(arr.pop() + arr.pop())
+        : tempArray.push(arr.pop() * arr.pop());
     }
- 
-    let action: boolean = true;
-    while (arr.length > 1){
-        arr = reduce(arr);
-    }
- 
-    return arr[0];
+    action = !action;
+    return tempArray;
+  }
+
+  let action: boolean = true;
+  while (arr.length > 1) {
+    arr = reduce(arr);
+  }
+
+  return arr[0];
 }
- 
-console.log(arrayConversion2([1,2,3,4,5,6,7,8]));
- 
+
+console.log(arrayConversion2([1, 2, 3, 4, 5, 6, 7, 8]));
+
+function arrayConversion3(inputArray: number[]): number {
+  let result: number = 0,
+    iterration: number = 0,
+    newArray = [],
+    newOld = [],
+    iterateIt = true;
+  do {
+    iterration++;
+    if (newOld.length == 0) {
+      newOld = inputArray;
+    }
+    if (iterration % 2 == 0) {
+      for (let i = 0; i < newOld.length; i = i + 2) {
+        newArray.push(newOld[i] * newOld[i + 1]);
+      }
+    } else {
+      for (let i = 0; i < newOld.length; i = i + 2) {
+        newArray.push(newOld[i] + newOld[i + 1]);
+      }
+    }
+    newOld = newArray;
+    newArray = [];
+    iterateIt = newOld.length > 1 ? true : false;
+  } while (iterateIt);
+  result = newOld[0];
+  return result;
+}
+
+console.log(arrayConversion3([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+NaN;
