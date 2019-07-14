23cd071f1659abc84a141449eb4bf96080a82c4f
Sone <sthaya@gmail.com>
2019-07-14 14:32:39 -0700

    updated solution

--- arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
+++ arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
@@ -1,46 +1,48 @@
 function arrayMaxConsecutiveSum(inputArray: number[], k: number): number {
-    let sum = 0;
-    let max = 0;
+  let sum = 0;
+  let max = 0;
 
-    for (let i = 0; i < k; i++){
-        sum += inputArray[i];
+  for (let i = 0; i < k; i++) {
+    sum += inputArray[i];
+  }
+
+  max = sum;
+
+  for (let i = k; i < inputArray.length; i++) {
+    sum -= inputArray[i - k];
+    sum += inputArray[i];
+    if (sum > max) {
+      max = sum;
     }
+  }
 
-    max = sum;
-
-    for (let i = k; i < inputArray.length; i++){
-        sum -= inputArray[i - k];
-        sum += inputArray[i];
-        if(sum > max){
-            max = sum;
-        }
-    }
-
-    return max;
+  return max;
 }
 
 console.log(arrayMaxConsecutiveSum([2, 3, 5, 1, 6], 2));
 
-// solution 2
+// solution 2 a
 
-function arrayMaxConsecutiveSum2(arr: number[], sizeOfElements: number): number {
-    
-    if (sizeOfElements > arr.length) return null;
-    
-    let maxSum: number = 0;
- 
-    for (let i = 0; i < sizeOfElements; i++){
-        maxSum += arr[i];
-    }
- 
-    let tempSum = maxSum;
- 
-    for (let i = 1; i < arr.length - 1; i++){
-        tempSum = tempSum - arr[i - 1] + arr[i + 1];
-        maxSum = Math.max(maxSum, tempSum);
-    }
- 
-    return maxSum;
+function arrayMaxConsecutiveSum2(
+  arr: number[],
+  sizeOfElements: number
+): number {
+  if (sizeOfElements > arr.length) return null;
+
+  let maxSum: number = 0;
+
+  for (let i = 0; i < sizeOfElements; i++) {
+    maxSum += arr[i];
+  }
+
+  let tempSum = maxSum;
+
+  for (let i = 1; i < arr.length - 1; i++) {
+    tempSum = tempSum - arr[i - 1] + arr[i + 1];
+    maxSum = Math.max(maxSum, tempSum);
+  }
+
+  return maxSum;
 }
- 
-console.log(arrayMaxConsecutiveSum2([2,3,5,1,6], 2));
+
+console.log(arrayMaxConsecutiveSum2([2, 3, 5, 1, 6], 2));
