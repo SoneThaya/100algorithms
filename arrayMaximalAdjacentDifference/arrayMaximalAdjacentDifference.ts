2944eeed1f0dd940fb464b1e3ee5b7f2bcebc659
Sone <sthaya@gmail.com>
2019-07-15 17:16:25 -0700

    updated solution

--- arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
+++ arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
@@ -1,13 +1,13 @@
 function arrayMaximalAdjacentDifference(inputArray: number[]): number {
-    let maxDiff = Math.abs(inputArray[0] - inputArray[1]);
+  let maxDiff = Math.abs(inputArray[0] - inputArray[1]);
 
-    for (let i = 0; i < inputArray.length; i++){
-        let absoluteDiff = Math.abs(inputArray[i - 1] - inputArray[i]);
+  for (let i = 0; i < inputArray.length; i++) {
+    let absoluteDiff = Math.abs(inputArray[i - 1] - inputArray[i]);
 
-        maxDiff = absoluteDiff > maxDiff ? absoluteDiff : maxDiff;
-    }
+    maxDiff = absoluteDiff > maxDiff ? absoluteDiff : maxDiff;
+  }
 
-    return maxDiff;
+  return maxDiff;
 }
 
 console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
@@ -15,18 +15,17 @@ console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
 //solution 2
 
 function arrayMaximalAdjacentDifference2(arr: number[]): number {
- 
-    if (arr.length == 2) return Math.abs(arr[0] - arr[1]);
- 
-    let maxDifference: number = Math.abs(arr[0] - arr[1]);
- 
-    for (let i = 1; i < arr.length; i++){
-        maxDifference = Math.max(maxDifference, Math.abs(arr[i - 1] - arr[i]));
-    }
- 
-    return maxDifference;
+  if (arr.length == 2) return Math.abs(arr[0] - arr[1]);
+
+  let maxDifference: number = Math.abs(arr[0] - arr[1]);
+
+  for (let i = 1; i < arr.length; i++) {
+    maxDifference = Math.max(maxDifference, Math.abs(arr[i - 1] - arr[i]));
+  }
+
+  return maxDifference;
 }
- 
+
 console.log(arrayMaximalAdjacentDifference2([2, 4, 0, 1]));
 console.log(arrayMaximalAdjacentDifference2([2, 8, 0, -2]));
 console.log(arrayMaximalAdjacentDifference2([2, 5, 7, 9]));
