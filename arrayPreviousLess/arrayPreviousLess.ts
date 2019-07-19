4fdae549a34686fa7667ef9db8aff62bb89f0f10
Sone <sthaya@gmail.com>
2019-07-18 17:28:36 -0700

    updated solution

--- arrayPreviousLess/arrayPreviousLess.ts
+++ arrayPreviousLess/arrayPreviousLess.ts
@@ -1,18 +1,18 @@
 function arrayPreviousLess(items: number[]): number[] {
-    const lessThanList: number[] = [];
+  const lessThanList: number[] = [];
 
-    for (let i = items.length - 1; i >= 0; i--){
-        for(let j = i; j >= 0; j--){
-            if(items[i] > items[j]){
-                lessThanList.unshift(items[j]);
-                break;
-            } else if (j === 0){
-                lessThanList.unshift(-1);
-            }
-        }
+  for (let i = items.length - 1; i >= 0; i--) {
+    for (let j = i; j >= 0; j--) {
+      if (items[i] > items[j]) {
+        lessThanList.unshift(items[j]);
+        break;
+      } else if (j === 0) {
+        lessThanList.unshift(-1);
+      }
     }
+  }
 
-    return lessThanList;
+  return lessThanList;
 }
 
 console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
@@ -20,25 +20,19 @@ console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
 // solution 2
 
 function arrayPreviousLess2(items: number[]): number[] {
-    let arr = [];
- 
- 
-    for (let i = 0; i < items.length; i++) {
-        
-        if(items[i] > items[i-1]) {
-         
-          arr.push(items[i-1])
-        }
-        else{
-            arr.push(-1);
-        }
-        
-        
+  let arr = [];
+
+  for (let i = 0; i < items.length; i++) {
+    if (items[i] > items[i - 1]) {
+      arr.push(items[i - 1]);
+    } else {
+      arr.push(-1);
     }
- 
-    return arr;
+  }
+
+  return arr;
 }
- 
+
 console.log(arrayPreviousLess2([3, 5, 2, 4, 5]));
 console.log(arrayPreviousLess2([0, 9, 4, 11, 9]));
 console.log(arrayPreviousLess2([52, 95, 21, 3, 55]));
