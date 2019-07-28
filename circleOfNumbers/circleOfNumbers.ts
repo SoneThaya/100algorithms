fc6e032894f7ba90d54f771a95741d52c242fef5
Sone <sthaya@gmail.com>
2019-07-27 21:14:41 -0700

    updated solution

--- circleOfNumbers/circleOfNumbers.ts
+++ circleOfNumbers/circleOfNumbers.ts
@@ -1,16 +1,16 @@
 function circleOfNumbers(n: number, firstNumber: number): number {
-    const numArray = [];
-    const halfWay = n / 2;
+  const numArray = [];
+  const halfWay = n / 2;
 
-    for (let i = 0; i < n; i++){
-        numArray.push(i);
-    }
+  for (let i = 0; i < n; i++) {
+    numArray.push(i);
+  }
 
-    if (firstNumber < halfWay){
-        return numArray[firstNumber + halfWay];
-    }
+  if (firstNumber < halfWay) {
+    return numArray[firstNumber + halfWay];
+  }
 
-    return numArray[firstNumber - halfWay];
+  return numArray[firstNumber - halfWay];
 }
 
 console.log(circleOfNumbers(10, 2));
@@ -18,8 +18,8 @@ console.log(circleOfNumbers(10, 2));
 // solution 2
 
 const circleOfNumbers2 = (n, firstNumber) => {
-    const half = n / 2;
-    return (half + firstNumber) % n;
+  const half = n / 2;
+  return (half + firstNumber) % n;
 };
 console.log(circleOfNumbers2(10, 2)); // 7
-console.log(circleOfNumbers2(10, 7)); 
+console.log(circleOfNumbers2(10, 7));
